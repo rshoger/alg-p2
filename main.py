ee94dc6ba129eddad8a3ce013276a2815dfb6155
rshoger <rossshoger6152@mail.linnbenton.edu>
2017-02-05 18:11:47 -0800

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import argparse
+
 from os.path import splitext
 from timeit import timeit
-import argparse
+from itertools import izip_longest as zip2
 
 ## Parse arguments ##
 parser = argparse.ArgumentParser(description='Test some algorithms.')
@@ -12,24 +14,23 @@ args = parser.parse_args()
 ## Algorithm functions ##
 # Slow change algorithm
 def changeslow(amounts):
-    pass
+    return 0
 
 # Greedy algorithm
 def changegreedy(amounts):
-    pass
+    return 0
 
 # Dynamic programming algorithm
 def changedp(amounts):
-    pass
+    return 0
 
 ## Test class ##
 class some_test:
     def __init__(self, input_filename):
-        # Store a set of sets
         self.sets = []
+        self.process_input(input_filename)
 
         # File-names
-        input_filename = input_filename
         output_filename = splitext(input_filename)[0] + 'change.txt'
         time_results_filename = 'timing_results.txt'
 
@@ -42,6 +43,15 @@ class some_test:
         self.output_file.close()
         self.time_results_file.close()
 
+    def process_input(self, filename):
+        with open(filename, 'r') as f:
+            # Unzip the file into two sets
+            z_1, z_2 = zip2(*zip(f,f))
+
+            # Create the test set
+            for i, l in enumerate(z_1):
+                self.sets.append([l.replace('\n',''), z_2[i].replace('\n','')])
+
     def test_sets(self, f):
         if not self.sets:
             self.sets.append([1,2,3,4])
@@ -50,16 +60,21 @@ class some_test:
 
         for i in self.sets:
             # Call the function on the set
-            print('Calling ' + f.__name__ + '...')
-            self.test_algorithm(f, i)
+            print('\nCalling ' + f.__name__ + '...\n--------\n Change: ' 
+                  + i[0] + '\n Amount: ' + i[1])
+
+            # Run and store results for algorithm
+            r = self.test_algorithm(f, i[0])
+
+            # Write to stdout and file handle
+            print(' Returned: ' + str(r))
+            self.output_file.write(i[0] + '\n' + str(r) + '\n')
 
             # Perform timing
-            t = self.time_algorithm(f, i)
+            t = self.time_algorithm(f, i[0])
 
-            # Print results to stdout
-            print('time: ' + str(t) + ' seconds')
-
-            # Write results to file
+            # Print results to stdout and file handle
+            print('\n Time: ' + str(t) + ' seconds')
             self.time_results_file.write(str(t)+'\n')
 
     def test_algorithm(self, f, s=[1,2,3,4]):
